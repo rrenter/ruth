1e2e08ec703786d320f74b5fc5f533bd5a60cbe3
rrenter <ruth_renter@hotmail.com>
2016-10-05 14:01:03 +0300

    Update test.java

--- test/test.java
+++ test/test.java
@@ -1,5 +1,5 @@
 public class test {
     public static void main(String args[]) {
-        System.out.println("Hello World! I need help I guess!");
+        System.out.println("Hello World! I need help I guess! I might be smarter by now!");
     }
 }
